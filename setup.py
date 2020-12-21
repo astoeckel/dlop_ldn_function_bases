ee31165b351574d2bb714e1868cee3a08962ef36
Andreas Stöckel <astoecke@uwaterloo.ca>
2020-12-21 02:59:27 -0500

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,9 @@ setup(
     license='License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
+    install_requires=[
+        'numpy >= 1.19',
+        'scipy >= 1.5',
+    ],
 )
 
